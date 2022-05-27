ea5b920b2992fe639077b631b73360005445b358
Shah Akshar <72061051+Shahakshar@users.noreply.github.com>
2022-05-27 10:44:22 +0530

    some comments added.
    I have added some comments in the constants file that is inside the utils package.

--- app/src/main/java/com/example/weather79/utils/Constants.kt
+++ app/src/main/java/com/example/weather79/utils/Constants.kt
@@ -2,8 +2,16 @@ package com.example.weather79.utils
 
 object Constants {
 
+    // you can add openweather api url that you have purchase
+    // example like- https://api.openweathermap.org/
     const val BASE_URL = "<api.openweathermap.org>"
+    
+    // when you made account on it you get free apikey using it to get some weather info 
+    // use that api key here.
+    // example like- hsdfigsodfhjaishefhskad
     const val API_KEY = "<API_KEY>"
+    
+    // this is for image rendering so that image appears.
     const val IMAGE_URL = "https://openweathermap.org/img/wn/"
 
 }
